f678a77cacb6bdae0d89dbc744fffb6e4f7abbea
margaretta hemmings <mbcc1905@hotmail.com>
2018-05-07 22:47:25 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,9 @@
 var index = require("../index.js")
 
 describe('what-is-a-test', () => {
-  describe('My great test', () => {
-    it('is really really great"', () => {
-      expect(index.platypus).toEqual('Harry')
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(index.name).toEqual('Susan')
     })
   })
 
